731a4642d58e5f878f2471a5609cec912a38ff5b
exvacuo <xvacuo@gmail.com>
2026-05-12 17:22:07 +0200

    feat: implemented resize, crop_blue_frame

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-#from asl_detector.data import load_data, preprocess_data
-
+from asl_detector.data.dataloader import load_data
+from asl_detector.data.preprocess_data import preprocess
+import cv2
 class Pipeline:
     """Class that encapsulates the pipeline for the ASL classification task"""
     def __init__(self) -> None:
+        self.data = None
         return
 
     def run(self):
         data = load_data()
-        print(data)
-        #preprocess_data()
+
+    def run_baseline(self):
+        self.data = load_data()
+        for i in range(len(self.data[0])):
+            self.data[0][i] = preprocess(self.data[0][i], 64)
+        return self.data
+        
+        
 
 if __name__ == '__main__':
     pipeline = Pipeline()
-    pipeline.run()
-    
+    pipeline.run_baseline()
+    print(pipeline.data[0][0])
+
+    cv2.imshow('Preprocessed Image', pipeline.data[0][0])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
