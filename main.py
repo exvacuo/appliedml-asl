a00b76d2fc975dc7a1e3b7fd2263432dd108f0ef
exvacuo <xvacuo@gmail.com>
2026-05-12 16:23:42 +0200

    wip: load data

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-# This is a sample Python script.
+#from asl_detector.data import load_data, preprocess_data
 
-def hello_world():
-    return "Hello, World!"
+class Pipeline:
+    """Class that encapsulates the pipeline for the ASL classification task"""
+    def __init__(self) -> None:
+        return
 
+    def run(self):
+        data = load_data()
+        print(data)
+        #preprocess_data()
 
 if __name__ == '__main__':
-    hello_world()
+    pipeline = Pipeline()
+    pipeline.run()
+    
