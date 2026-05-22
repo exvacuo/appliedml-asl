f5a9cef3183a7b9e4c2b9065458cbaeaa525be7b
MHE2006 <merijnstreefkerk@gmail.com>
2026-05-22 15:34:59 +0200

    added classifier head in model class

--- asl_detector/models/mobilenetv2.py
+++ asl_detector/models/mobilenetv2.py
@@ -12,12 +12,21 @@ class ASLMobilenetv2():
         )
         self.base_model.trainable = False
         self.build_classifier(num_classes)
+    
     def build_classifier(self, num_classes: int = 29):
-        ## TODO build the classifier head
-        ...
+        inputs = tf.keras.Input(shape=(224, 224, 3))
+        x = self.base_model(inputs, training=False)
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Dropout(self.dropout_rate)(x)
+        outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+        self.model = tf.keras.Model(inputs, outputs)
         
-    def unfreeze_top_n_layers(self, model: tf.keras.Model, n: int):
+    def unfreeze_top_n_layers(self, n: int):
         """Phase 2: unfreeze the last N layers of the backbone."""
         self.base_model.trainable = True
         for layer in self.base_model.layers[:-n]:
             layer.trainable = False
+
+model = ASLMobilenetv2()
+model.model.summary()
