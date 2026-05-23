2dc9ad809279b21a290f7bb57ef85d0d1828081f
exvacuo <xvacuo@gmail.com>
2026-05-23 14:08:16 +0200

    feat: implement fastapi root route

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-import tensorflow as tf
-
-from asl_detector.data.dataloader import load_data
-from asl_detector.models.knn import run_kfold, train_final_model, evaluate_on_test
-from asl_detector.features import extract_features
-from asl_detector.data.preprocess_data import preprocess_knn, find_near_duplicates
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-class Pipeline:
-    """Class that encapsulates the pipeline for the ASL classification task."""
+app = FastAPI()
 
-    def __init__(self) -> None:
-        self.train = None
-        self.val = None
-        self.test = None
-
-    def run_baseline(self, extract_features):
-        """Load data, preprocess for KNN, and trains with Kfold cross validation"""
-        train, val, test = load_data(baseline=True)
-        X_train, y_train = extract_features(dataset=train)
-        K_fold = run_kfold(X_train, y_train)
-        
-        return train_results
-
-    
-
-
-if __name__ == "__main__":
-    
-    image_dir = "data/raw/test/asl_alphabet_test"
-    duplicates = find_near_duplicates(image_dir)
-
-    print("Near-duplicate images:", len(duplicates))
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
